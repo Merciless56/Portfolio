6ac33e81fa8504add8820ef41c63d507cb7eb67c
Azizjon Vorisov <aziz.pochta@mail.ru>
2023-11-09 16:59:23 +0500

    step 1

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const menulinks = document.querySelectorAll('.nav_link[data-goto]');
+
+if (menulinks.length > 0) {
+    menulinks.forEach(menulink => {
+        menulink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e) {
+        const menulink = e.target;
+        if (menulink.dataset.goto && document.querySelector(menulink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menulink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth",
+            });
+            e.preventDefault()
+        }
+    }
+}
+
